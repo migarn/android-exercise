33c52d1257a192c64376fd1358b2a0610f94f3a6
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-04-26 11:41:53 +0200

    Methods for button actions implemented

--- Counter/app/src/main/java/com/example/counter/MainActivity.java
+++ Counter/app/src/main/java/com/example/counter/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +17,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increaseTextView(View view) {
+        counter++;
+        setTextViewValue(counter);
     }
 
     public void decreaseTextView(View view) {
+        counter--;
+        setTextViewValue(counter);
     }
 
     public void clearTextView(View view) {
+        counter = 0;
+        setTextViewValue(counter);
+    }
+
+    private void setTextViewValue(int counter) {
+        TextView textView = (TextView) findViewById(R.id.counter_view);
+        textView.setText(counter);
     }
 }
