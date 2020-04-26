16b3c2e0080ff3909943a3f0a07a3d14b4f50bff
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-04-26 11:10:16 +0200

    Button action created

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -2,13 +2,25 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        // Set the text view as the activity layout
+        setContentView(textView);
+
     }
 }
